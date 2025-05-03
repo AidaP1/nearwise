78fc846ca4f35d91b6dabea361549996f7de8cd8
Aidan <pilgrim.aidan@gmail.com>
2025-05-03 23:09:23 +0100

    fixed SQLALCHEMY_DATABASE_URI in config.py

--- app/config.py
+++ app/config.py
@@ -2,17 +2,17 @@ import os
 
 class DefaultConfig:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev-key')
-    DATABASE_URL = os.getenv('PROD_DATABASE_URL', 'sqlite:///myapp.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('PROD_DATABASE_URL', 'sqlite:///myapp.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProdConfig(DefaultConfig):
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev-key')
-    DATABASE_URL = os.getenv('PROD_DATABASE_URL', 'sqlite:///myapp.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('PROD_DATABASE_URL', 'sqlite:///myapp.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     pass
 
 class StagingConfig(DefaultConfig):
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev-key')
-    DATABASE_URL = os.getenv('STAGING_DATABASE_URL', 'sqlite:///myapp.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('STAGING_DATABASE_URL', 'sqlite:///myapp.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     pass
